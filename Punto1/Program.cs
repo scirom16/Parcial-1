6d85ef3872289f7b536e6b983afa756ba29f886c
scirom <sebastian.cirom16@gmail.com>
2024-03-08 21:00:11 -0500

    Solución punto 1

--- Punto1/Program.cs
+++ Punto1/Program.cs
@@ -1,4 +1,13 @@
-﻿// Declaración de variables
+﻿//Realizar un algoritmo con C# donde se solucione el siguiente problema: Un vendedor recibe un
+//sueldo base de $XXXX. Además, recibe una comisión del 10% sobre el valor total de cada venta que
+//realiza. El vendedor desea saber:
+// ¿Cuánto dinero obtendrá por concepto de comisiones por las tres ventas que realiza en el
+//mes?
+// ¿Cuál es el total que recibirá en el mes tomando en cuenta su sueldo base y comisiones?
+// ¿Cuál fue la venta que le generó la mayor comisión?
+// ¿Cuál fue el promedio de las comisiones que recibió por cada venta?
+
+// Declaración de variables
 double comision = 0.1;
 double v1, v2, v3;
 string mensaje = " Fue la venta que te generó mayor comisión";
