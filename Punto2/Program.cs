25b87edd7e794689f41396edbf4a649f1f7f33b0
scirom <sebastian.cirom16@gmail.com>
2024-03-08 21:01:27 -0500

    Solución Punto 2

--- Punto2/Program.cs
+++ Punto2/Program.cs
@@ -1,4 +1,10 @@
-﻿//Declaración de variables
+﻿//Un tren sale de la estación “A” a las 10:00 AM y viaja a una velocidad constante de 80 km/h. Un
+//segundo tren sale de la misma estación a las 11:00 AM y viaja en la misma dirección a una velocidad
+//constante de 100 km/h. ¿A qué hora el segundo tren alcanzará al primer tren? Mostrar esta hora en
+//pantalla
+
+
+//Declaración de variables
 double tren1 = 80, tren2 = 100;
 //Declaro una variable "Distancia" para denotar la distancia que hay entre los 2 trenes, el valor de esta variable va a ser la operación
 //Distancia = velocidad * tiempo
